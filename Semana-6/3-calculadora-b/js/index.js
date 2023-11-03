3334fca1a9e1112cfd4e8fc137bc58812ed0e5ff
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-11-02 23:57:20 -0500

    termiando calculadora

--- Semana-6/3-calculadora-b/js/index.js
+++ Semana-6/3-calculadora-b/js/index.js
@@ -5,11 +5,35 @@ const documentReady = () => {
 
     const calculate = (e) => {
         e.preventDefault();
-        const number1 = document.getElementById('number1').value;
+        const number1 = +document.getElementById('number1').value;
         const operator = document.getElementById('operator').value;
-        const number2 = document.getElementById('number2').value;
+        const number2 = +document.getElementById('number2').value;
+        const resultContainer = document.getElementById('resultContainer');
 
-        alert(+number1 + +number2);
+        let result = 0;
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                if (number2 !== 0) {
+                    result = number1 / number2;
+                } else {
+                    resultContainer.innerText = 'No se puede dividir entre 0';
+                    return;
+                }
+                break;
+            default:
+                break;
+        };
+        resultContainer.innerText = result;
     };
 
     calculatorForm.addEventListener('submit', calculate);
