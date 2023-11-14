7f5074897af3e948f3676ac6fbba06dec3fbf6c6
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-11-13 21:10:15 -0500

    agregando header

--- Semana-9/1-patita-feliz-r/src/App.js
+++ Semana-9/1-patita-feliz-r/src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
+import Header from './components/sections/Header';
 
 function App() {
   return (
-    <h1>Patita Feliz</h1>
+    <Header />
   );
 }
 
---
+++ Semana-9/1-patita-feliz-r/src/components/sections/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className="py-3">
+            <div className="container d-flex flex-column gap-1">
+            <h2 className="text-info text-center fs-2 m-0">Patita Feliz</h2>
+            <h1 className="text-primary text-center fs-5 m-0">La mejor veterinaria del Peru</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
