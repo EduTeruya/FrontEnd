094f246b8f65a732079563eae6a124bba6bb475b
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-11-13 23:45:44 -0500

    aprendiendo react

--- Semana-9/1-patita-feliz-r/src/App.js
+++ Semana-9/1-patita-feliz-r/src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
 import Header from './components/sections/Header';
+import Footer from './components/sections/Footer';
+import Appointments from './pages/Appointments';
 
 function App() {
+
+  const company = {
+    name: 'Patita Feliz',
+    slogan: '🐹 La mejor veterinaria del Peru 🐹'
+  };
+
+  const credits = {
+    year: new Date().getFullYear(),
+    author: 'Edu Teruya'
+  };
+
   return (
-    <Header />
+    <>
+      <Header company={company} />
+      <main>
+      <Appointments />
+      </main>
+      <Footer credits={credits} />
+    </>
   );
 }
 
