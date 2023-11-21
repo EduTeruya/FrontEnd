bfa39153b4f14a58d1798b3ee2caadf361c9847a
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-11-20 21:53:21 -0500

    agregando iconos

--- Semana-10/e-commerce-store/src/components/sections/Header.jsx
+++ Semana-10/e-commerce-store/src/components/sections/Header.jsx
@@ -1,15 +1,38 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.webp';
+import { useRef } from 'react';
 
 const Header = () => {
+
+    const header = useRef();
+    const modal = useRef();
+
+    const showMenu = () => {
+        header.current.classList.add('header--menu');
+        modal.current.classList.add('modal--show');
+    };
+
+    const hideMenu = () => {
+        header.current.classList.remove('header--menu');
+        modal.current.classList.remove('modal--show');
+    };
+
     return (
-        <header className="header">
+        <header
+            className="header"
+            ref={header}
+        >
+
             <nav className="nav">
                 <div className="container f-elements f-elements--header">
-                    <a>
+                    <Link to="/">
                         <img src={logo} alt="logo e-commerce" className='nav__logo' width="32" height="32" />
-                    </a>
-                    <div className="modal modal--header">
+                    </Link>
+                    <div
+                        className="modal modal--header"
+                        ref={modal}
+                        onClick={hideMenu}
+                    >
                         <ul className="list list--header">
                             <li><Link to="/productos" className="list__link">Productos</Link>
                             </li>
@@ -19,15 +42,20 @@ const Header = () => {
                             </li>
                             <li><Link to="/contacto" className="list__link">Contacto</Link>
                             </li>
+                            <li className='icon icon--close'>
+                                ❌
+                            </li>
                         </ul>
                     </div>
                     <div className='f-elements f-elements--center'>
                         <select>
-                            <option value="value1">🌑</option>
-                            <option value="value2">☀</option>
+                            <option value="value1">🌚</option>
+                            <option value="value2">🌞</option>
                             <option value="value3">💻</option>
                         </select>
-                        <button className='icon'>
+                        <button className='icon'
+                            onClick={showMenu}
+                        >
                             🍔
                         </button>
                     </div>
